6f5d20c568984a2e0f0c0d777c8808adc671dbe6
eeeseohyun <eeeseohyun@gmail.com>
2023-06-04 19:19:10 +0900

    week12_js_이서현

--- team_js/week11/이서현/tumblbug.js
+++ team_js/week11/이서현/tumblbug.js
@@ -1,7 +1,13 @@
-$carouselSlide = document.querySelector('.carouselslide');
-$order = document.querySelector('.order b');
-$buttonleft = document.querySelector('.left');
-$buttonright = document.querySelector('.right');
+const $carouselSlide = document.querySelector('.carouselslide');
+const $order = document.querySelector('.order b');
+const $buttonleft = document.querySelector('.left');
+const $buttonright = document.querySelector('.right');
+const $time = document.querySelector('#time');
+const $category = document.querySelector('#first');
+const $hoverWrap= document.querySelector('.hoverWrap');
+const $search =document.querySelector('.search');
+const $clicksearch =document.querySelector('.clicksearch');
+const $x = document.querySelector('#x');
 let count=0;
 setInterval(()=>{
     let incount=(-766)*count;
@@ -32,3 +38,29 @@ $buttonleft.addEventListener("click",(event)=>{
     }
     $carouselSlide.style.transform=`translateX(${incount}px)`;
 });
+setInterval(()=>{
+    let data = new Date();
+    let year = data.getFullYear()-(parseInt(data.getFullYear()/100)*100);
+    let month = data.getMonth();
+    let day = data.getDay();
+    let hours = data.getHours();
+    let minutes = data.getMinutes();
+    let seconds = data.getSeconds();
+    let timestring = `${year}. ${month}. ${day} ${hours} : ${minutes} : ${seconds} 기준`;
+    $time.textContent= timestring;
+    console.log(timestring);
+},1000);
+$category.addEventListener('mouseover',hover);
+function hover(){
+    $hoverWrap.style.visibility='visible';
+}
+$category.addEventListener('mouseleave',leave)
+function leave(){
+    $hoverWrap.style.visibility='hidden';
+}
+$search.addEventListener('click',()=>{
+    $clicksearch.style.visibility ='visible';
+})
+$x.addEventListener('click',()=>{
+    $clicksearch.style.visibility='hidden';
+})
