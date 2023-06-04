22e4ae42444cdd3fe22971c22b11da0ff15c0ac8
taewook <taewook02@mju.ac.kr>
2023-06-04 12:58:42 +0900

    added fixed navbar

--- team_js/week11/남태욱/script.js
+++ team_js/week11/남태욱/script.js
@@ -5,9 +5,10 @@ const $carouselIndexIndicator = document.querySelector('.indexIndicator strong')
 
 const $timeIndicator = document.querySelector('.main-nav-TimeIndicator');
 
-let carouselIndex = 0;
+const $navBar = document.querySelector('.header-nav-wrap');
+const $mainLayout = document.querySelector('.main-layout');
 
-const translateCarousel = () => {}
+let carouselIndex = 0;
 
 $prevButton.addEventListener('click', () => {
 	if (carouselIndex === 0) {
@@ -29,6 +30,7 @@ $nextButton.addEventListener('click', () => {
 	$carouselImage.style.transform = `translate3d(-${100 * carouselIndex}%, 0, 0)`;
 });
 
+// 캐러셀 이미지 자동 이동
 let autoCarousel = setInterval(() => {
 	carouselIndex += 1;
 	if (carouselIndex === 4) {
@@ -39,6 +41,7 @@ let autoCarousel = setInterval(() => {
 	$carouselImage.style.transform = `translate3d(-${100 * carouselIndex}%, 0, 0)`;
 }, 3500);
 
+// 메인 네비게이션 바 시간 업데이트
 let displayTime = setInterval(() => {
 	let currentTime = new Date();
 
@@ -52,3 +55,14 @@ let displayTime = setInterval(() => {
 
 	$timeIndicator.innerText = `${year}.${month}.${day} ${hours}:${minutes}:${seconds} 기준`;
 } , 1000);
+
+// 스크롤시 상단 네비게이션 바 고정
+window.onscroll = () => {
+	if(window.scrollY > $navBar.offsetTop) {
+		$navBar.classList.add('fixNav');
+		$mainLayout.classList.add('fixMain');
+	} else {
+		$navBar.classList.remove('fixNav');
+		$mainLayout.classList.remove('fixMain');
+	}
+}
